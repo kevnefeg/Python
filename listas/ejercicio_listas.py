dafccfa1a40ead101e811a5f314b94a7055ba309
kevnefeg <62613609+kevnefeg@users.noreply.github.com>
2020-11-02 11:10:14 -0600

    python

---
+++ listas/ejercicio_listas.py
@@ -0,0 +1,12 @@
+#practicas de listas
+
+dias=["lunes","martes","miercoles","jueves","viernes","sabado","domingo"]
+
+dia = dias[0]
+print(dia)
+
+dia = dias[5]
+print(dia)
+
+dia = dias[6]
+print(dia)
